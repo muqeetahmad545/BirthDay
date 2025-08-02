d5a12995b37afe536fe959f16b31a4461279c2f5
muqeetahmad545 <inoxentuni1@gmail.com>
2025-08-02 14:57:59 +0500

    puhs

--- builder/init.js
+++ builder/init.js
@@ -1,17 +1,17 @@
-const axios = require("axios").default;
-const path = require("path");
-const fs = require("fs");
-const setPic = require("./getPic");
-const genIndex = require("./genIndex");
+const axios = require('axios').default;
+const path = require('path');
+const fs = require('fs');
+const setPic = require('./getPic');
+const genIndex = require('./genIndex');
 const {
   generateMarkupLocal,
   generateMarkupRemote,
-} = require("./generateMarkup");
+} = require('./generateMarkup');
 
-require("dotenv").config();
+require('dotenv').config();
 
-if (!process.env.NAME) throw new Error("Please specify NAME in environment.");
-if (!process.env.PIC) throw new Error("Please specify PIC in environment.");
+if (!process.env.NAME) throw new Error('Please specify NAME in environment.');
+if (!process.env.PIC) throw new Error('Please specify PIC in environment.');
 
 const picPath = process.env.PIC;
 const msgPath = process.env.SCROLL_MSG;
@@ -19,11 +19,11 @@ const msgPath = process.env.SCROLL_MSG;
 //Local initialization
 const setLocalData = async () => {
   try {
-    const pic = path.join(__dirname, "../local/", picPath);
-    let markup = "";
+    const pic = path.join(__dirname, '../local/', picPath);
+    let markup = '';
     if (msgPath) {
-      const text = fs.readFileSync(path.join(__dirname, "../local/", msgPath), {
-        encoding: "utf-8",
+      const text = fs.readFileSync(path.join(__dirname, '../local/', msgPath), {
+        encoding: 'utf-8',
       });
       markup = generateMarkupLocal(text);
     }
@@ -38,19 +38,19 @@ const setLocalData = async () => {
 const setRemoteData = async () => {
   try {
     let res = await axios.get(picPath, {
-      responseType: "arraybuffer",
+      responseType: 'arraybuffer',
     });
     const pic = res.data;
-    let markup = "";
+    let markup = '';
     if (msgPath) {
-      const article = msgPath.split("/").pop();
+      const article = msgPath.split('/').pop();
       res = await axios.get(
         `https://api.telegra.ph/getPage/${article}?return_content=true`
       );
       const { content } = res.data.result;
       markup = content.reduce(
         (string, node) => string + generateMarkupRemote(node),
-        ""
+        ''
       );
     }
     await setPic(pic);
@@ -60,6 +60,6 @@ const setRemoteData = async () => {
   }
 };
 
-if (process.argv[2] === "--local") setLocalData();
-else if (process.argv[2] === "--remote") setRemoteData();
-else console.log("Fetch mode not specified.");
+if (process.argv[2] === '--local') setLocalData();
+else if (process.argv[2] === '--remote') setRemoteData();
+else console.log('Fetch mode not specified.');
--- src/js/animation.js
+++ src/js/animation.js
@@ -1,33 +1,33 @@
 //jshint esversion:6
 
-const button = document.querySelector(".btn"),
-  darkroom = document.querySelector(".darkroom"),
-  giftroom = document.querySelector(".giftroom"),
-  hallway = document.querySelector(".hallway"),
-  room = document.querySelector(".empty-room"),
-  flash = document.querySelector(".flash");
+const button = document.querySelector('.btn'),
+  darkroom = document.querySelector('.darkroom'),
+  giftroom = document.querySelector('.giftroom'),
+  hallway = document.querySelector('.hallway'),
+  room = document.querySelector('.empty-room'),
+  flash = document.querySelector('.flash');
 
 // These are the text elements that hold messages to be displayed in the respective screes
 
-const blackText = document.querySelectorAll(".bb-text"), // msgs in the dark room scene
-  giftText = document.querySelectorAll(".gift-text"), // msgs in the gift scene
-  hallText = document.querySelectorAll(".hall-text"), // msgs in the hallway scene
-  roomText = document.querySelectorAll(".room-text"), // msgs in empty room scene
-  CTAtext = document.querySelector(".btn-ref");
+const blackText = document.querySelectorAll('.bb-text'), // msgs in the dark room scene
+  giftText = document.querySelectorAll('.gift-text'), // msgs in the gift scene
+  hallText = document.querySelectorAll('.hall-text'), // msgs in the hallway scene
+  roomText = document.querySelectorAll('.room-text'), // msgs in empty room scene
+  CTAtext = document.querySelector('.btn-ref');
 
 //Elements in the card page
 
-const frames = document.querySelectorAll(".frame"),
-  msgWindow = document.querySelector(".scroll"), // this one has the message frame in [0] and card fram in [1]
-  msg = document.querySelector(".text"); // the Message para
+const frames = document.querySelectorAll('.frame'),
+  msgWindow = document.querySelector('.scroll'), // this one has the message frame in [0] and card fram in [1]
+  msg = document.querySelector('.text'); // the Message para
 
 //Sfx files
 
-const light = document.querySelector(".switch-aud"),
-  blast = document.querySelector(".blast-aud"),
-  door = document.querySelector(".door-aud"),
-  haunt = document.querySelector(".haunt-aud"),
-  music = document.querySelector(".hbd-aud");
+const light = document.querySelector('.switch-aud'),
+  blast = document.querySelector('.blast-aud'),
+  door = document.querySelector('.door-aud'),
+  haunt = document.querySelector('.haunt-aud'),
+  music = document.querySelector('.hbd-aud');
 
 //  readMsg() displays the paras in each scene successively. It takes an array of the para elements as input.
 
@@ -36,11 +36,11 @@ const readMsg = (text) => {
     // this loop goes through all the text msg paras
     setTimeout(() => {
       // A timeout of 5s ia applied to all text elements so that appear successively one after the other
-      text[i].classList.add("read"); // this adds a fadeIn-fadeOut animation to elements
+      text[i].classList.add('read'); // this adds a fadeIn-fadeOut animation to elements
       if (i === text.length - 1) {
         // this ensures that the button appears only after the last text is displayed.
-        button.style.display = "inline-block";
-        CTAtext.style.display = "block";
+        button.style.display = 'inline-block';
+        CTAtext.style.display = 'block';
       }
     }, 5000 * i);
   }
@@ -49,10 +49,10 @@ const readMsg = (text) => {
 // transition() is animation for change from one scene to another. It takes the current scene div element as input.
 
 const transition = (currentScene) => {
-  currentScene.classList.add("fade-in");
-  currentScene.style.opacity = "0";
-  button.style.display = "none";
-  CTAtext.style.display = "none";
+  currentScene.classList.add('fade-in');
+  currentScene.style.opacity = '0';
+  button.style.display = 'none';
+  CTAtext.style.display = 'none';
 };
 
 //Animation Code
@@ -63,12 +63,12 @@ const transition = (currentScene) => {
 */
 
 export const animate = function () {
-  CTAtext.innerHTML = "Click the Light Bulb.";
+  CTAtext.innerHTML = '👩‍❤️‍👨 Click the Light Bulb. Mari wife 👩‍❤️‍👨';
 
   readMsg(blackText);
 
-  button.addEventListener("click", function () {
-    if (button.classList.contains("switch")) {
+  button.addEventListener('click', function () {
+    if (button.classList.contains('switch')) {
       /* 
               When the switch is pressed, the black div will wipe out and the backgroung scene with no 
               elements will appear, signifying that the lights are turned on and the room is empty. Then 
@@ -78,14 +78,14 @@ export const animate = function () {
 
       light.play();
       transition(darkroom);
-      CTAtext.innerHTML = "Click the Door";
+      CTAtext.innerHTML = '🚪 Click the Door My Life Line ❤️';
       setTimeout(function () {
-        button.classList.add("door-out");
-        button.classList.remove("switch");
-        darkroom.style.display = "none";
+        button.classList.add('door-out');
+        button.classList.remove('switch');
+        darkroom.style.display = 'none';
         readMsg(roomText);
       }, 4000);
-    } else if (button.classList.contains("door-out")) {
+    } else if (button.classList.contains('door-out')) {
       /* 
               when the door is pressed, scene changes to cemetry. Again, the msg will be displayed, after 
               which, the user will be asked to come inside and the button with door will appear again.
@@ -96,12 +96,12 @@ export const animate = function () {
       setTimeout(function () {
         haunt.play();
         haunt.loop = true;
-        button.classList.add("door-in");
-        button.classList.remove("door-out");
-        room.style.display = "none";
+        button.classList.add('door-in');
+        button.classList.remove('door-out');
+        room.style.display = 'none';
         readMsg(hallText);
       }, 4000);
-    } else if (button.classList.contains("door-in")) {
+    } else if (button.classList.contains('door-in')) {
       /* 
               when the door is pressed, scene changes to the gift room. Again, the msg will be displayed, after 
               which, the user will be asked to open the gift and the button with gift will appear.
@@ -109,14 +109,14 @@ export const animate = function () {
 
       door.play();
       transition(hallway);
-      CTAtext.innerHTML = "Click the Gift";
+      CTAtext.innerHTML = 'Click the Gift';
       setTimeout(function () {
-        button.classList.add("gift");
-        button.classList.remove("door-in");
-        hallway.style.display = "none";
+        button.classList.add('gift');
+        button.classList.remove('door-in');
+        hallway.style.display = 'none';
         readMsg(giftText);
       }, 4000);
-    } else if (button.classList.contains("gift")) {
+    } else if (button.classList.contains('gift')) {
       /* 
               when the gift is pressed, the gift scene vanishes and the white div fades slowly giving a sense 
               of explosion. After that, the message frame appears and moves up until the message completes. Then,
@@ -125,17 +125,17 @@ export const animate = function () {
 
       haunt.pause();
       blast.play();
-      giftroom.style.display = "none";
+      giftroom.style.display = 'none';
       transition(flash);
 
       music.loop = true;
       music.play();
 
       if (!process.env.SCROLL_MSG) {
-        frames[0].style.display = "flex";
+        frames[0].style.display = 'flex';
         setTimeout(() => {
-          frames[0].classList.add("appear");
-          frames[0].style.opacity = "1";
+          frames[0].classList.add('appear');
+          frames[0].style.opacity = '1';
         }, 1500);
         return;
       }
@@ -144,33 +144,33 @@ export const animate = function () {
       const readTime =
         parseInt(
           getComputedStyle(document.documentElement).getPropertyValue(
-            "--readTime"
+            '--readTime'
           )
         ) + 5;
 
-      frames[1].style.display = "flex";
+      frames[1].style.display = 'flex';
 
       setTimeout(() => {
-        frames[1].classList.add("appear");
-        frames[1].style.opacity = "1";
-        msg.classList.add("move-up");
+        frames[1].classList.add('appear');
+        frames[1].style.opacity = '1';
+        msg.classList.add('move-up');
       }, 1500);
 
       setTimeout(() => {
-        msg.style.transform = "translateY(-100%)";
-        flash.style.display = "none";
+        msg.style.transform = 'translateY(-100%)';
+        flash.style.display = 'none';
       }, 5000);
 
       setTimeout(() => {
-        msgWindow.classList.add("fade-in");
-        msgWindow.style.opacity = "0";
+        msgWindow.classList.add('fade-in');
+        msgWindow.style.opacity = '0';
       }, readTime * 1000);
 
       setTimeout(() => {
-        frames[1].style.display = "none";
-        frames[0].style.display = "flex";
-        frames[0].classList.add("appear");
-        frames[0].style.opacity = "1";
+        frames[1].style.display = 'none';
+        frames[0].style.display = 'flex';
+        frames[0].classList.add('appear');
+        frames[0].style.opacity = '1';
       }, (readTime + 3) * 1000);
     }
   });
